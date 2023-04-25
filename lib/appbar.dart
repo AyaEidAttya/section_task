be9dc4bd4eea8b0d73f2e8bef47ecbf190698c9d
My Name <myemail@example.com>
2023-04-25 22:52:58 +0200

    add back color to app bar

---
+++ lib/appbar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Appbar extends StatefulWidget {
+  const Appbar({super.key});
+
+  @override
+  State<Appbar> createState() => _AppbarState();
+}
+
+class _AppbarState extends State<Appbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("data"),
+        backgroundColor: Color.fromARGB(255,255, 255, 255),
+      ),
+    );
+  }
+}
